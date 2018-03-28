ccbc209e7e003aff6c9fb93f2a515f1f95b8e0cf
SiewYan <siew.yan.hoh@cern.ch>
2018-03-29 01:06:33 +0200

    rename

--- submit.sh
+++ submit.sh
@@ -18,10 +18,10 @@
 ./install.py --carddir Cards/fPort_Maj_LO_ld1_Mphi_Mchi_13TeV-madgraph --proc 404 --med 500 --dm 400 --glu 0 --gld 1 --runcms runcmsgrid_LO.sh
 
 #405 -> darkHiggs Mono-hs
-./install.py --carddir Cards/darkHiggs_Monohs_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --proc 405 --gq 0.25 --gdm 1.001 --hdm 50 --med 2000 --dm 100 --runcms runcmsgrid_LO.sh 
+./install.py --carddir Cards/darkHiggs_Monohs_LO_MZprime_Mhs_Mchi_13TeV-madgraph --proc 405 --gq 0.25 --gdm 1.001 --hdm 50 --med 2000 --dm 100 --runcms runcmsgrid_LO.sh 
 
 #406 -> darkHiggs Mono-Z'
-./install.py --carddir Cards/darkHiggs_MonoZp_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --proc 406 --gq 0.25 --gdm 1.001 --hdm 150 --med 2000 --dm 50 --runcms runcmsgrid_LO.sh
+./install.py --carddir Cards/darkHiggs_MonoZp_LO_MZprime_Mhs_Mchi_13TeV-madgraph --proc 406 --gq 0.25 --gdm 1.001 --hdm 150 --med 2000 --dm 50 --runcms runcmsgrid_LO.sh
 
 #407 -> Non Thermal Dark Matter
 ./install.py --carddir Cards/NonthDM_MonoJet_Mx_l1_l2_13TeV-madgraph --proc 407 --glam1 0.25 --glam2 1.001 --med 2000 -runcms runcmsgrid_LO.sh 
