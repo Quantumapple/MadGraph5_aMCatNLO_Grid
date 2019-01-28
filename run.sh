7f4dab96b52a7edae1dd9994e3f21033a04513e2
Jongho Lee <jongho.lee@cern.ch>
2019-01-28 22:12:16 +0100

    New processes

--- run.sh
+++ run.sh
@@ -44,7 +44,60 @@
 
 #BBbarDM-matchedJet
 #./install.py --carddir Cards/BBbarDM400Match1jet_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 50 --queue 1nd
-./install.py --carddir Cards/BBbarDM400Match1jet_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --queue 1nd
-./install.py --carddir Cards/BBbarDM400Match1jet_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 --queue 1nd
+#./install.py --carddir Cards/BBbarDM400Match1jet_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --queue 1nd
+#./install.py --carddir Cards/BBbarDM400Match1jet_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 --queue 1nd
 
 #./install.py --carddir Cards/BBbarDM400Match1jet_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 50 --dm 50 --med 3000
+
+##MonojetMatch1Jets
+#./install.py --carddir Cards/MonojetMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 50 -dm 300 --med 2000 --queue 1nd
+#./install.py --carddir Cards/MonojetMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 50 -dm 400 --med 2500 --queue 1nd
+#./install.py --carddir Cards/MonojetMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 -dm 100 --med 3000 --queue 1nd
+#./install.py --carddir Cards/MonojetMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 -dm 200 --med 3000 --queue 1nd 
+#./install.py --carddir Cards/MonojetMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 -dm 150 --med 500 --queue 1nd
+
+##MonoZDM
+#./install.py --carddir Cards/MonoZDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 50 --queue 1nd
+#./install.py --carddir Cards/MonoZDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --queue 1nd
+#./install.py --carddir Cards/MonoZDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 --queue 1nd
+#./install.py --carddir Cards/MonoZDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 50 --med 495 --dm 250 --queue 1nd
+#./install.py --carddir Cards/MonoZDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 --med 495 --dm 250 --queue 1nd
+#./install.py --carddir Cards/MonoZDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --med 495 --dm 250 --queue 1nd
+
+##MonoZDM + 1 Jet matching
+#./install.py --carddir Cards/MonoZMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 50 --queue 1nd
+#./install.py --carddir Cards/MonoZMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --queue 1nd
+#./install.py --carddir Cards/MonoZMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 --queue 1nd
+#./install.py --carddir Cards/MonoZMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 50 --med 495 --dm 250 --queue 1nd
+#./install.py --carddir Cards/MonoZMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 --med 495 --dm 250 --queue 1nd
+#./install.py --carddir Cards/MonoZMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --med 495 --dm 250 --queue 1nd
+
+#./install.py --carddir Cards/MonoZMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 --med 2500 --dm 250 --queue 1nd
+#./install.py --carddir Cards/MonoZMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 --med 2500 --dm 300 --queue 1nd
+#./install.py --carddir Cards/MonoZMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 --med 2500 --dm 400 --queue 1nd
+#./install.py --carddir Cards/MonoZMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 --med 2500 --dm 50 --queue 1nd
+#./install.py --carddir Cards/MonoZMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --med 3000 --dm 200 --queue 1nd
+
+##MonoWDM
+#./install.py --carddir Cards/MonoWDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 50 --queue 1nd
+#./install.py --carddir Cards/MonoWDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --queue 1nd
+#./install.py --carddir Cards/MonoWDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 --queue 1nd
+#./install.py --carddir Cards/MonoWDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 50 --med 495 --dm 250 --queue 1nd
+#./install.py --carddir Cards/MonoWDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 --med 495 --dm 250 --queue 1nd
+#./install.py --carddir Cards/MonoWDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --med 495 --dm 250 --queue 1nd
+
+##MonoWMatch1JetsDM
+#./install.py --carddir Cards/MonoWMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 50 --queue 1nd
+#./install.py --carddir Cards/MonoWMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --queue 1nd
+#./install.py --carddir Cards/MonoWMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 --queue 1nd
+#./install.py --carddir Cards/MonoWMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 50 --med 495 --dm 250 --queue 1nd
+#./install.py --carddir Cards/MonoWMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 90 --med 495 --dm 250 --queue 1nd
+#./install.py --carddir Cards/MonoWMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --med 495 --dm 250 --queue 1nd
+
+#./install.py --carddir Cards/MonoWMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --med 2500 --dm 250 --queue 1nd
+#./install.py --carddir Cards/MonoWMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 50 --med 3000 --dm 150 --queue 1nd
+#./install.py --carddir Cards/MonoWMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 50 --med 3000 --dm 400 --queue 1nd
+#./install.py --carddir Cards/MonoWMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --med 3000 --dm 100 --queue 1nd
+#./install.py --carddir Cards/MonoWMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --med 3000 --dm 150 --queue 1nd
+#./install.py --carddir Cards/MonoWMatch1JetsDM_LO_MZprime_Mhs_Mchi_gSM-0p25_gDM-1p0_th_0p01_13TeV-madgraph --gq 0.25 --gdm 1.001 --hdm 70 --med 3000 --dm 200 --queue 1nd
+
